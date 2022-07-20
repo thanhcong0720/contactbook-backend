87af68a5312f24a841f4839fc9d635c9320e9e18
hieunguyen1104 <hieu.nguyen@southasiaservices.com>
2022-07-20 22:57:38 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 const contactsRouter = require("./app/route/contact.route");
+const ApiError = require("./app/api-error");
 
 const app = express();
 
@@ -9,6 +10,17 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/contacts", contactsRouter);
+
+// handle 404 request
+app.use((req, res, next) => {
+  return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+  return res.status(err.statusCode || 500).json({
+    message: err.message || "Internal server error",
+  });
+});
 
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to contact book application." });
